10f3f748e71d5183331c15706dba831a2491bcdf
Linka <Polina153@mail.ru>
2024-01-21 15:32:49 +0400

    1 TODO added

--- src/main/kotlin/OOP.kt
+++ src/main/kotlin/OOP.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     val human3:Human = Human("Daniel",70,89)
     val human4:Human = Human("Josh",46,115)
 
-    val mediumAge =( human.age + human1.age + human2.age + human3.age + human4.age ) / 5
+    val mediumAge =( human.age + human1.age + human2.age + human3.age + human4.age ) / 5//TODO вот здесь попробовать сделать не мануально, а программно. (то есть людей посчитать должна программа, как минимум, а как максимум - и пробежаьтсья по всем созданным людям она должна сама, и если мы создадим еще людей - они учтутся)
     println(mediumAge)*/
 
 /*//    about dog

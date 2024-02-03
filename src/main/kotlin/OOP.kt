8ef3986a754d78787501606313f985390505e59f
PATYA <patysik@mail.ru>
2024-02-04 01:39:15 +0300

    добавила метод вычисления сред.арифм.возрастов

--- src/main/kotlin/OOP.kt
+++ src/main/kotlin/OOP.kt
@@ -1,12 +1,23 @@
+import java.util.stream.IntStream
+
 fun main(args: Array<String>) {
 
 //    вычисление среднего арифмет. возраста людей
-    val human:Human = Human("Mike",23,79 )
-    val human1:Human = Human("Evan",37,82)
-    val human2:Human = Human("Alex",17,65)
-    val human3:Human = Human("Daniel",70,89)
-    val human4:Human = Human("Josh",46,115)
+    var listOfHuman:MutableList<Human> = mutableListOf()
+    listOfHuman.add(Human("John",24,73))
+    listOfHuman.add(Human("Evan",37,82))
+    listOfHuman.add(Human("Alex",17,65))
+    listOfHuman.add(Human("Daniel",70,89))
+    listOfHuman.add(Human("Josh",46,115))
 
+    fun averageAge():Double {
+        var sum = 0.0
+        for(human in listOfHuman){
+           sum += human.age }
+        val averageOfAge = sum / listOfHuman.size
+        return averageOfAge
+    }
+    println( averageAge() )
 
 /*//    about dog
     val dog:Dog = Dog("Bobik", "ovcharka", 13)
